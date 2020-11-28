625001be4da64db885a2e05e38c64dc10cc07425
Akshay Dagade <akdagade@gmail.com>
2020-11-27 19:28:03 -0500

    Function Defination for 2's complement compare

--- source/TreeEvaluator.h
+++ source/TreeEvaluator.h
@@ -14,6 +14,9 @@ public:
     static helib::Ctxt evaluate_decision_tree(helib::Ctxt input_vector);
 
     static helib::Ctxt calculate_result(helib::Ctxt decisions[], helib::Ctxt leaf_nodes[], helib:: Ctxt ctxt_1);
+
+    static helib::Ctxt compareCtxt(helib::Ctxt xCtxt, helib::Ctxt yCtxt);
+
 };
 
 
